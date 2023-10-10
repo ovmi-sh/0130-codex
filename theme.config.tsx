1683ef3fedba82c52fae64d20c7bdfb02f5b3e90
micovi <micleaovidiul@gmail.com>
2023-10-10 15:01:27 +0300

    replace docs with codex

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,12 +3,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Obsidian 13 Order Codex</span>,
   project: {
-    link: 'https://github.com/ovmi-sh/0130-docs',
+    link: 'https://github.com/ovmi-sh/0130-codex',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/ovmi-sh/0130-docs',
+  docsRepositoryBase: 'https://github.com/ovmi-sh/0130-codex',
   footer: {
     text: 'Nextra Docs Template',
   },
