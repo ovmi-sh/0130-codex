5ab4fa335a9e406fc9cc942a4fd120f65b77a273
micovi <micleaovidiul@gmail.com>
2023-10-10 15:00:54 +0300

    initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,17 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Obsidian 13 Order Codex</span>,
+  project: {
+    link: 'https://github.com/ovmi-sh/0130-docs',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/ovmi-sh/0130-docs',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
